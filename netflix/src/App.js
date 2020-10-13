ca309e663d9f3b2b79af91e018a1e9e236da77c8
ashokkumarnaidu999@gmail.com <ashokkumarnaidu999@gmail.com>
2020-10-13 18:32:45 +0530

    add jumbotron and footer components

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import Jumbotron from "./components/jumbotron"
+import { FooterContainer } from './containers/footer';
+import { JumbotronContainer } from './containers/jumbotron';
+
 function App() {
   return (
-    
-       <Jumbotron />
-    
+    <>
+      <JumbotronContainer/>
+      <FooterContainer />
+    </>
+       
   );
 }
 
